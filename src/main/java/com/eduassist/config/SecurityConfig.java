1482f6abcc5c43746185f02e20d74ed2f47a4363
Gamma <202300300370@mail.sdu.edu.cn>
2025-06-16 14:21:20 +0800

    demo1，实现了后端的基本功能，等待测试

--- src/main/java/com/eduassist/config/SecurityConfig.java
+++ src/main/java/com/eduassist/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.eduassist.config;
 
-import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
